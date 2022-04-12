decd4e6464f4ad82c9f51bd9dcda23c62d2a4a93
LocTranDinh <dinhlockt02@gmail.com>
2022-04-12 21:57:11 +0700

    Add request user info

--- app/src/main/java/xyz/daijoubuteam/foodshoppingapp/MainActivity.kt
+++ app/src/main/java/xyz/daijoubuteam/foodshoppingapp/MainActivity.kt
@@ -9,6 +9,8 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.*
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
@@ -19,19 +21,24 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.launch
 import xyz.daijoubuteam.foodshoppingapp.authentication.AuthActivity
+import xyz.daijoubuteam.foodshoppingapp.authentication.RequestUserInfoActivity
 import xyz.daijoubuteam.foodshoppingapp.authentication.VerifyActivity
 import xyz.daijoubuteam.foodshoppingapp.client.feeds.FeedsFragment
 import xyz.daijoubuteam.foodshoppingapp.client.home.HomeFragment
 import xyz.daijoubuteam.foodshoppingapp.client.orders.OrdersFragment
 import xyz.daijoubuteam.foodshoppingapp.client.saved.SavedFragment
 import xyz.daijoubuteam.foodshoppingapp.databinding.ActivityMainBinding
+import xyz.daijoubuteam.foodshoppingapp.repositories.AuthRepository
 
 class MainActivity : AppCompatActivity() {
 
     private val auth = Firebase.auth
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+    private val authRepository = AuthRepository()
+    private val isUserRegisterInformation = MutableLiveData<Boolean?>(null)
 
     private fun addAuthStateListener() {
         auth.addAuthStateListener {
@@ -42,6 +49,13 @@ class MainActivity : AppCompatActivity() {
             } else if (!it.currentUser!!.isEmailVerified) {
                 val intent = Intent(this, VerifyActivity::class.java)
                 intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(intent)
+            }
+        }
+
+        isUserRegisterInformation.observe(this){
+            if(it == false){
+                val intent = Intent(this@MainActivity, RequestUserInfoActivity::class.java)
                 startActivity(intent)
             }
         }
@@ -57,5 +71,16 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
         val navController = Navigation.findNavController(this, R.id.navHomeFragment)
         NavigationUI.setupWithNavController(binding.bottomNavigation, navController)
+        lifecycleScope.launch {
+            auth.currentUser?.let {
+                val userResult = authRepository.getUserByUid(it.uid)
+                if (userResult.isFailure || userResult.getOrNull() == null) {
+                    auth.signOut()
+                    return@launch
+                }
+                val user = userResult.getOrNull()!!
+                isUserRegisterInformation.value = user.isUserRegisterInformation
+            }
+        }
     }
 }
--- app/src/main/java/xyz/daijoubuteam/foodshoppingapp/authentication/forgetpassword/ForgetPasswordViewModelFactory.kt
+++ app/src/main/java/xyz/daijoubuteam/foodshoppingapp/authentication/forgetpassword/ForgetPasswordViewModelFactory.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
 class ForgetPasswordViewModelFactory: ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if(modelClass.isAssignableFrom(ForgetPasswordViewModel::class.java)){
             return ForgetPasswordViewModel() as T
