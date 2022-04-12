d6efbb4ed48ec6b2d88c2de0b057607284869231
LocTranDinh <dinhlockt02@gmail.com>
2022-04-12 17:41:31 +0700

    Change Navigation to AndroidX

--- app/src/main/java/xyz/daijoubuteam/foodshoppingapp/MainActivity.kt
+++ app/src/main/java/xyz/daijoubuteam/foodshoppingapp/MainActivity.kt
@@ -9,10 +9,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavController
-import androidx.navigation.NavHost
-import androidx.navigation.NavHostController
-import androidx.navigation.findNavController
+import androidx.navigation.*
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
@@ -53,29 +50,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        val homeFragment = HomeFragment()
-        val feedsFragment = FeedsFragment()
-        val ordersFragemnt = OrdersFragment()
-        val savedFragment = SavedFragment()
-
-        binding.bottomNavigation.setOnItemSelectedListener {
-            when(it.itemId) {
-                R.id.ic_home -> makeCurrentFragment(homeFragment)
-                R.id.ic_feeds -> makeCurrentFragment(feedsFragment)
-                R.id.ic_orders -> makeCurrentFragment(ordersFragemnt)
-                R.id.ic_saved -> makeCurrentFragment(savedFragment)
-            }
-            true
-        }
         addAuthStateListener()
     }
 
-    private fun makeCurrentFragment(fragment:Fragment) {
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.navHomeFragment,fragment)
-            commit()
-        }
+    override fun onStart() {
+        super.onStart()
+        val navController = Navigation.findNavController(this, R.id.navHomeFragment)
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navController)
     }
-
 }
