0ef0a4a19638e2c62a61a79e860ff56f4ceb57ec
LocTranDinh <dinhlockt02@gmail.com>
2022-04-14 21:01:13 +0700

    Fix adapter error

--- app/src/main/java/xyz/daijoubuteam/foodshoppingapp/adapter/OrdersApdater.kt
+++ app/src/main/java/xyz/daijoubuteam/foodshoppingapp/adapter/OrdersApdater.kt
@@ -2,22 +2,25 @@ package xyz.daijoubuteam.foodshoppingapp.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import xyz.daijoubuteam.foodshoppingapp.R
 import xyz.daijoubuteam.foodshoppingapp.databinding.ItemOrderBinding
 import xyz.daijoubuteam.foodshoppingapp.model.Orders
 
-class OrdersApdater: ListAdapter<Orders, OrdersApdater.OrdersViewHolder>(DiffCallBack) {
-    class OrdersViewHolder(private var binding: ItemOrderBinding) : RecyclerView.ViewHolder(binding.root) {
+class OrdersApdater : ListAdapter<Orders, OrdersApdater.OrdersViewHolder>(DiffCallBack) {
+    class OrdersViewHolder(private var binding: ItemOrderBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun bind(order: Orders) {
             binding.order = order
             binding.executePendingBindings()
         }
     }
 
-    companion object DiffCallBack: DiffUtil.ItemCallback<Orders>() {
+    companion object DiffCallBack : DiffUtil.ItemCallback<Orders>() {
         override fun areItemsTheSame(oldItem: Orders, newItem: Orders): Boolean {
             return oldItem == newItem
         }
@@ -25,11 +28,11 @@ class OrdersApdater: ListAdapter<Orders, OrdersApdater.OrdersViewHolder>(DiffCal
         override fun areContentsTheSame(oldItem: Orders, newItem: Orders): Boolean {
             return oldItem.titleOrders == newItem.titleOrders
         }
-
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OrdersViewHolder {
-        return OrdersViewHolder(ItemOrderBinding.inflate(LayoutInflater.from(parent.context)))
+        val binding = ItemOrderBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return OrdersViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: OrdersViewHolder, position: Int) {
