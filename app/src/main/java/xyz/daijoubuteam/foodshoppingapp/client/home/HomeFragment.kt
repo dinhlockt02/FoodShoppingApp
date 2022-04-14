7910ad6dbb17a81bcabb62eff4b64fab4b8d131a
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2022-04-14 12:38:11 +0700

    Add home ui

--- app/src/main/java/xyz/daijoubuteam/foodshoppingapp/client/home/HomeFragment.kt
+++ app/src/main/java/xyz/daijoubuteam/foodshoppingapp/client/home/HomeFragment.kt
@@ -1,60 +1,121 @@
 package xyz.daijoubuteam.foodshoppingapp.client.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import xyz.daijoubuteam.foodshoppingapp.R
+import xyz.daijoubuteam.foodshoppingapp.databinding.FragmentHomeBinding
+import xyz.daijoubuteam.foodshoppingapp.model.Category
+import xyz.daijoubuteam.foodshoppingapp.model.Cuisine
+import xyz.daijoubuteam.foodshoppingapp.model.Eatery
+import xyz.daijoubuteam.foodshoppingapp.model.SlideItem
+import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    var page: ViewPager? = null
+    var tabLayout: TabLayout? = null
+    var listItems: ArrayList<SlideItem>? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val binding = DataBindingUtil.inflate<FragmentHomeBinding>(inflater, R.layout.fragment_home, container, false)
+
+        //List popular eateries and near by eateries
+        val listPopularEatery : ArrayList<Eatery> = ArrayList()
+        listPopularEatery.add(Eatery(1, "Mon 1", "ABC CDE", 15, 2.0, 5, R.drawable.img_rectangle15))
+        listPopularEatery.add(Eatery(3, "Mon 1", "ABC CDE", 15, 2.0, 5, R.drawable.img_rectangle15_2))
+        listPopularEatery.add(Eatery(3, "Mon 1", "ABC CDE", 15, 2.0, 5, R.drawable.img_rectangle15))
+
+        val adapterEatery: AdapterEatery = AdapterEatery()
+        adapterEatery.setData(listPopularEatery)
+        binding.recyPopularEatery.adapter = adapterEatery
+        binding.recyPopularEatery.layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.HORIZONTAL, false)
+
+        binding.recyNearByEatery.adapter = adapterEatery
+        binding.recyNearByEatery.layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.HORIZONTAL, false)
+        //List categories
+        val listCategories : ArrayList<Category> = ArrayList()
+        listCategories.add(Category(1, "Promos", R.drawable.img_discount))
+        listCategories.add(Category(2, "Meal", R.drawable.img_steak))
+        listCategories.add(Category(3, "Drink", R.drawable.img_cocktail))
+        listCategories.add(Category(4, "Fastfood", R.drawable.img_burger))
+        listCategories.add(Category(5, "Snack", R.drawable.img_macaroons))
+
+        val adapterCategory: AdapterCategory = AdapterCategory()
+        adapterCategory.setData(listCategories)
+        binding.recyCategories.adapter = adapterCategory
+        binding.recyCategories.layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.HORIZONTAL, false)
+
+        //List cuisines
+        val listCuisines : ArrayList<Cuisine> = ArrayList()
+        listCuisines.add(Cuisine(1, "New Zealander", R.drawable.img_newzealand))
+        listCuisines.add(Cuisine(2, "Italian", R.drawable.img_italian))
+        listCuisines.add(Cuisine(3, "Greece", R.drawable.img_greece))
+
+        val adapterCuisine: AdapterCuisine = AdapterCuisine()
+        adapterCuisine.setData(listCuisines)
+        binding.recyPopularCuisine.adapter = adapterCuisine
+        binding.recyPopularCuisine.layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.HORIZONTAL, false)
+
+        //Slider
+        page = binding.myPager
+        tabLayout = binding.myTablayout
+
+        listItems = ArrayList()
+        listItems!!.add(
+            SlideItem(
+                1,
+                R.drawable.img_rectangle12,
+            )
+        )
+        listItems!!.add(
+            SlideItem(
+                2,
+                R.drawable.img_rectangle12,
+            )
+        )
+        listItems!!.add(
+            SlideItem(
+                3,
+                R.drawable.img_rectangle12,
+            )
+        )
+        listItems!!.add(
+            SlideItem(
+                4,
+                R.drawable.img_rectangle12,
+            )
+        )
+
+        val adapterSlide: AdapterSlide = AdapterSlide(this.requireContext(), listItems!!)
+        page!!.adapter = adapterSlide
+
+        // The_slide_timer
+        tabLayout!!.setupWithViewPager(page, true)
+        setSliderTimer(2000,3000)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun setSliderTimer(delay: Long, period: Long){
+        lifecycleScope.launch {
+            delay(delay)
+            while(true){
+                if (page!!.currentItem < listItems!!.size - 1) {
+                    page!!.currentItem = page!!.currentItem + 1
+                } else page!!.currentItem = 0
+                delay(period)
             }
+        }
     }
 }
---
+++ app/src/main/java/xyz/daijoubuteam/foodshoppingapp/model/Category.kt
@@ -0,0 +1,7 @@
+package xyz.daijoubuteam.foodshoppingapp.model
+
+data class Category (
+    val id: Int,
+    val name: String,
+    val icon: Int
+)
