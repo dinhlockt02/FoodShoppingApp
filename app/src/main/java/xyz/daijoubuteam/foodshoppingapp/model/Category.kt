d28163e8204ff050c919cd160db3707f9334d341
quangnhat22 <wuangsotech@gmail.com>
2022-05-30 21:28:45 +0700

    update slide

--- app/src/main/java/xyz/daijoubuteam/foodshoppingapp/model/Category.kt
+++ app/src/main/java/xyz/daijoubuteam/foodshoppingapp/model/Category.kt
@@ -1,6 +1,9 @@
 package xyz.daijoubuteam.foodshoppingapp.model
 
+import com.google.firebase.firestore.DocumentId
+
 data class Category (
+    @DocumentId
     val id: String?=null,
     val name: String?=null,
     val icon: String?=null
